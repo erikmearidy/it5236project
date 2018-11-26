e4d613f67f4f51e6f6ca2f6879733d92f0be74a0
erikmearidy <42616954+erikmearidy@users.noreply.github.com>
2018-11-26 13:29:14 -0500

    Update credentials.php

--- website/include/credentials.php
+++ website/include/credentials.php
@@ -5,9 +5,9 @@ class Credentials {
 
 
     // Declare the credentials to the database
-   public $servername = "it5236db.cq3jrtlgrdly.us-east-1.rds.amazonaws.com";
-   public $serverusername = "em03389";
-   public $serverpassword = "it5236DBpass";
+   public $servername = "it5236db";
+   public $serverusername = "user";
+   public $serverpassword = "password";
    public $serverdb = "it5236";
 
 }
